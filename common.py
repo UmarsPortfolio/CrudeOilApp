f4fd1a0582cb205596a15b5cb1ace73491f6823c
UmarKhan <umarkhan314@outlook.com>
2020-11-12 20:04:22 -0600

    yann fixed the cron but the latest news wont load?

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
 
-#working_dir = Path(os.path.dirname(os.path.realpath(__file__)))
-working_dir = 'c:\prompt_root\CrudeOilApp'
+working_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+#working_dir = 'c:\prompt_root\CrudeOilApp'
 print(working_dir)
