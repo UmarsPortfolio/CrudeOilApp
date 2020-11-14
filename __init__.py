c959a7c6c52a8a8674b10ffe84ff10ce0420a2f9
UmarKhan <umarkhan314@outlook.com>
2020-11-13 22:07:19 -0600

    updated update script to not put redundant entries in db anymore

--- __init__.py
+++ __init__.py
@@ -398,7 +398,7 @@ def update_value(n_clicks,start_date,end_date,series):
 
 server = app.server
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
 
     print ('running')
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from __init__ import server as app
+from __init__ import server as application
 
 
 if __name__ == "__main__":
